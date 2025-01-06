e02f799cbc1044eb0fece26a1e88879489092d70
kacperekgamerek <kac.spyc@gmail.com>
2025-01-06 20:07:09 +0100

    dodanie obsługi rejestracji i logowania

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 def post_list(request):
     """Wyświetla listę wszystkich postów."""
@@ -49,3 +53,15 @@ def post_delete(request, post_id):
 
     # Jeśli to GET, wyświetlamy stronę potwierdzenia usunięcia
     return render(request, 'posts/post_confirm_delete.html', {'post': post})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse_lazy('post_list'))  # Możesz przekierować na stronę główną
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
